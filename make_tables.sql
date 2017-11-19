338648f8e9caefa04f8dd85e7b3a2a884778d80f
Khalah Jones-Golden <khasan222@gmail.com>
2017-11-19 14:12:54 -0400

    [MySQL] Fixed make tables query

--- make_tables.sql
+++ make_tables.sql
@@ -12,7 +12,7 @@ SET time_zone = "+00:00";
 
 
 CREATE TABLE `screenshots` (
-	`ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` text COLLATE utf8mb4_unicode_ci NOT NULL,
 	`file_path` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
 	`created_at` datetime DEFAULT CURRENT_TIMESTAMP
@@ -20,7 +20,7 @@ CREATE TABLE `screenshots` (
 
 
 CREATE TABLE `comparisons` (
-	`ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` text COLLATE utf8mb4_unicode_ci NOT NULL,
 	`screenshot1_id` bigint(20) UNSIGNED NOT NULL,
 	`screenshot2_id` bigint(20) UNSIGNED NOT NULL,
@@ -29,13 +29,3 @@ CREATE TABLE `comparisons` (
 	`contours_path` datetime DEFAULT CURRENT_TIMESTAMP,
 	`diff_path` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDV DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-ALTER TABLE `screenshots` (
-	ADD PRIMARY KEY(`ID`)
-)
-
-ALTER TABLE `comparisons` (
-	ADD PRIMARY KEY(`ID`),
-	ADD KEY `screenshot1_id` (`screenshot1_id`),
-	ADD KEY `screenshot2_id` (`screenshot2_id`);
-)
